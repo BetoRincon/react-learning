36e3962a6e1c2461f0e9898cfe744f195de7cf67
Beto Rincon <albertorinconbarajas@gmail.com>
2023-04-17 11:55:39 -0500

    implement repos on Layout

--- grid-react/src/LayoutFactory.tsx
+++ grid-react/src/LayoutFactory.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { InMemoryCategoryRepository } from './infrastructure/InMemoryCategoryRepository';
+import { InMemoryProductRepository } from './infrastructure/InMemoryProductRepository';
 import { Layout } from './sections/layout/Layout';
 
-const inMemoryCategoryRepository = new InMemoryCategoryRepository()
+const inMemoryCategoryRepository = new InMemoryCategoryRepository();
+const inMemoryProductRepository = new InMemoryProductRepository();
 
 
 export function LayoutFactory(){
 
     return(
-        <Layout inMemoryCategoryRepository={inMemoryCategoryRepository} />
+        <Layout
+            inMemoryCategoryRepository={inMemoryCategoryRepository}
+            inMemoryProductRepository={inMemoryProductRepository}
+        />
     );
 }
